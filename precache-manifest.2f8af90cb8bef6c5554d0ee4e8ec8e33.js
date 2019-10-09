86758037d193d4a4f8a36e39494c13d1d4d5537d
Nguyen Duy Anh <duyanh19962012@gmail.com>
2019-10-09 19:21:21 +0300

    Updates

---
+++ precache-manifest.2f8af90cb8bef6c5554d0ee4e8ec8e33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f0c28ae6933e508accf187b0ea0475",
+    "url": "/ReactJSwithTS/index.html"
+  },
+  {
+    "revision": "5a15f6306e07b8aa9fe6",
+    "url": "/ReactJSwithTS/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "23ecd80ae00227b0a5da",
+    "url": "/ReactJSwithTS/static/js/2.d5c6831e.chunk.js"
+  },
+  {
+    "revision": "5a15f6306e07b8aa9fe6",
+    "url": "/ReactJSwithTS/static/js/main.0f1f517f.chunk.js"
+  },
+  {
+    "revision": "1e9c75fd436550864592",
+    "url": "/ReactJSwithTS/static/js/runtime-main.e462bfe0.js"
+  }
+]);
